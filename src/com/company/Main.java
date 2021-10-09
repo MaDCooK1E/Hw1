8060e5cbbbea6b74c4e4ff052851d567df32d1ab
_MaD_CooKiE_ <yaroslavoodessa@gmail.com>
2021-10-09 11:49:34 +0300

    1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,18 +4,19 @@ public class Main {
 
     public static void main(String[] args) {
         int n = 10;
-        Fact(1, n);
+        fact(1, n);
 
     }
 
-    public static void Fact(int fact, int n) {
+    public static void fact(int fact, int n) {
         fact *= n--;
         if (n == 1) {
             System.out.println(fact);
         } else {
-            Fact(fact, n);
+            fact(fact, n);
         }
 
     }
 
 }
+
